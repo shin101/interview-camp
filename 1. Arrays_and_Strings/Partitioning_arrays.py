fa3917f74c8e9672876edb2b684c5414df8f0a06
shin101 <shinj0127@gmail.com>
2023-04-24 15:11:09 -0700

    done with partitioning arrays

--- 1. Arrays_and_Strings/Partitioning_arrays.py
+++ 1. Arrays_and_Strings/Partitioning_arrays.py
@@ -3,6 +3,33 @@
 # Assume the colors are given as numbers - 0 (Red), 1 (White) and 2 (Blue).
 
 
+
+def sort_marbles(marbles):
+    pivot = 1
+    low_boundary = 0
+    high_boundary = len(marbles)-1
+    i = 0
+
+    while i <= high_boundary:
+        if marbles[i] == pivot:
+            i+=1
+        elif marbles[i] > pivot:
+            marbles[i], marbles[high_boundary] = marbles[high_boundary], marbles[i]
+            high_boundary -=1
+        else:
+            marbles[i], marbles[low_boundary] = marbles[low_boundary], marbles[i]
+            low_boundary +=1
+            i+=1
+    return marbles
+
+
+
+
+print(sort_marbles([0,1,1,2,1,0,1,2])) # should return [0,0,1,1,1,1,2,2].
+
+
+
+
 # def sort_marbles(marbles):
 #     pivot = 1
 #     low_boundary = 0
---
+++ 6. Recursion and Backtracking/Permutations.py
@@ -0,0 +1,13 @@
+# Coin Change Problem: Given a set of coin denominations, print out the different ways you can make a target amount. You can use as many coins of each denomination as you like.
+
+# For example: If coins are [1,2,5] and the target is 5, output will be:
+
+# [1,1,1,1,1]
+
+# [1,1,1,2]
+
+# [1,2,2]
+
+# [5]
+
+# Variant of this problem: If you had to print just the count (instead of the actual combinations), there is a more efficient solution in the Dynamic Programming Section. This solution is also acceptable though.
