12a93f6caee07f51bf0a7f726c4cc783e7de7267
shin101 <kendras0127@gmail.com>
2023-07-16 17:36:07 +0900

    Prefix_sums

--- 2. Subarray_problems/Prefix_sums.py
+++ 2. Subarray_problems/Prefix_sums.py
@@ -2,14 +2,10 @@
 
 # For example: Input = [2,4,-2,1,-3,5,-3], X = 5 --> Result = [2,4,-2,1].  Return the starting and ending indices of the subarray. Return null if array empty
 
+# ---Problem I struggled with, try again at a later time---
 
 def prefix_sum(input, target):
     pass
-
-print(prefix_sum([2,4,-2,1,-3,5,-3], 5)) # should return [0,3]
-print(prefix_sum([-1,2,2,-4,2,-1,4], 0)) # should return [1,3]
-print(prefix_sum([2, 1, 3, -1, -3, 7, -3], 1)) # should return [1]
-
 
 
 
@@ -19,31 +15,6 @@ print(prefix_sum([2, 1, 3, -1, -3, 7, -3], 1)) # should return [1]
 # if map contains sum then return map.get(sum)+1, i
 # map.add(sum, i) if none of the if conditions are true
 
-
-
-# ------------------------------------------------------------
-
-# my solution
-
-# def prefix_sum(input, target):
-#     dic = {} # same as cumulative sum
-#     # dic = {2:0, 6:1, 4:2, 5:3, ...} sum, idx
-#     sum = 0
-
-#     for idx, num in enumerate(input):
-#         sum += num
-#         if sum == target:
-#             return [0,idx]
-#         if sum in dic:
-#             return [dic[sum]+1, idx]
-#         dic[sum] = idx 
-#     return None
-
-# print(prefix_sum([2,4,-2,1,-3,5,-3], 5)) # should return [0,3]
-# # cumulative sum {2:0, 6:1, 4:2, 5:3, 2:4, 7:5, 4:6}
-# print(prefix_sum([-1,2,2,-4,2,-1,4], 0)) # should return [1,3]
-# print(prefix_sum([2, 1, 3, -1, -3, 7, -3], 1)) # should return [1]
-# # cumulative sum {2:0, 3:1, 6:2, 5:3, 2:4, 9:5, 6:6}
 
 
 # --------------------------------
--- 7. Linked List/Append_Function.py
+++ 7. Linked List/Append_Function.py
@@ -7,49 +7,106 @@
 
 # Note: Odd and Even here refer to the node's position, not value.
 
+# # ----------------------------------------------------------
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
 
-# class Node():
-#     def __init__(self,value):
-#         self.value = value
-#         self.next = None
+def oddEvenList(head):
+    pass
 
-# def getOddEven(head):
-#     if not head:
+# Test case
+def create_linked_list(values):
+    # Helper function to create a linked list from a list of values
+    if not values:
+        return None
+
+    head = Node(values[0])
+    curr = head
+    for value in values[1:]:
+        curr.next = Node(value)
+        curr = curr.next
+
+    return head
+
+
+def get_linked_list_values(head):
+    # Helper function to get the values of a linked list
+    values = []
+    curr = head
+    while curr:
+        values.append(curr.value)
+        curr = curr.next
+    return values
+
+# Test case
+head = create_linked_list([1,2,3,4,5])
+result = oddEvenList(head)
+result_values = get_linked_list_values(result)
+print(result_values)
+# ----------------------------------------------------------
+                    # WORKING SOLUTION
+# ----------------------------------------------------------
+
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+# def oddEvenList(head):
+#     if head == None or head.next == None or head.next.next == None:
 #         return head
-#     odd_pointer = current = head
-#     even_pointer = head.next
+    
+#     odd = curr = head
+#     even = evenhead = head.next
 #     idx = 1
 
-#     while current:
-#         if idx > 2 and idx % 2 != 0:
-#             odd_pointer.next = current
-#             odd_pointer = odd_pointer.next
-#         elif idx > 2 and idx % 2 == 0:
-#             even_pointer.next = current
-#             even_pointer = even_pointer.next
-
-#         current = current.next
-#         idx+=1
+#     while curr:
+#         # because we already defined first odd and even, we don't care abt idx 0 and 1
+#         if idx > 2 and idx % 2 !=0 :
+#             odd.next = curr
+#             odd = odd.next
+#         elif idx >2 and idx %2 == 0:
+#             even.next = curr
+#             even = even.next
+        
+#         curr = curr.next
+#         idx += 1
     
-#     even_pointer.next = None
-#     odd_pointer
+#     even.next = None
+#     odd.next = evenhead
+
 #     return head
 
 
+# # Test case
+# def create_linked_list(values):
+#     # Helper function to create a linked list from a list of values
+#     if not values:
+#         return None
 
-    
+#     head = Node(values[0])
+#     curr = head
+#     for value in values[1:]:
+#         curr.next = Node(value)
+#         curr = curr.next
 
-# ll = Node(1)
-# ll.next = Node(2)
-# ll.next.next = Node(3)
-# ll.next.next.next = Node(4)
-# ll.next.next.next.next = Node(5)
-
-# # call getOddEven function and print the results
-# print(getOddEven(ll))
-
-# should return : Result 1: 1 3 5
-# Result 2 : 2 4
+#     return head
 
 
+# def get_linked_list_values(head):
+#     # Helper function to get the values of a linked list
+#     values = []
+#     curr = head
+#     while curr:
+#         values.append(curr.value)
+#         curr = curr.next
+#     return values
+
+# # Test case
+# head = create_linked_list([1,2,3,4,5])
+# result = oddEvenList(head)
+# result_values = get_linked_list_values(result)
+# print(result_values)
 
