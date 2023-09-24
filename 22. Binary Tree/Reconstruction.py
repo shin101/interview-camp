bdffd886c3c04d0135fd219d318a4c51bec02bd2
shin101 <kendras0127@gmail.com>
2023-09-24 18:01:31 -0400

    1 LC a day

---
+++ 22. Binary Tree/Reconstruction.py
@@ -0,0 +1,9 @@
+# 105. Construct Binary Tree from Preorder and Inorder Traversal
+# Medium
+
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+# HINT 
+# first value of preorder is guaranteed to be a root because thats how it works 
+# everything to the left of root in inorder is on left subtree, everything on the right is on right subtree 
+
