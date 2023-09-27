37b0e453686a81a2d8be4e9b1e775879e8f77098
shin101 <kendras0127@gmail.com>
2023-09-26 22:49:09 -0400

    subsequence

--- 1. Arrays_and_Strings/Traverse_reverse.py
+++ 1. Arrays_and_Strings/Traverse_reverse.py
@@ -1,4 +1,4 @@
-# Given an array of numbers, replace each even number with twoof the same number. e.g, [1,2,5,6,8, , , ,] -> [1,2,2,5,6,6,8,8].Assume that the array has the exact amount of space to accommodate the result.
+# Given an array of numbers, replace each even number with two of the same number. e.g, [1,2,5,6,8, , , ,] -> [1,2,2,5,6,6,8,8].Assume that the array has the exact amount of space to accommodate the result.
 
 
 
---
+++ 11. Dynamic Programming/Subsequence.py
@@ -0,0 +1,31 @@
+from typing import List
+# 300. Longest Increasing Subsequence
+
+# Given an integer array nums, return the length of the longest strictly increasing 
+# subsequence
+# This is a DP question
+
+
+# Input: nums = [10,9,2,5,3,7,101,18]
+# Output: 4
+# Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+
+# hint 
+# iterate backwards, need condition for increasing order, cache repeated work 
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums) # cache
+
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1+LIS[j])
+        
+        return max(LIS)
+    
+
+# test case
+sol = Solution()
+print(sol.lengthOfLIS([10,9,2,5,3,7,101,18])) # 4 
